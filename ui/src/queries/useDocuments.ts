257149cf502932c6c51e48447e0f6fe22d7374ed
Joe Khoobyar <fourheads@gmail.com>
2026-03-14 16:56:32 -0400

    EDM-5: autofile-ui: thumbnail loading should avoid memory leaks and use a cache

--- ui/src/queries/useDocuments.ts
+++ ui/src/queries/useDocuments.ts
@@ -1,6 +1,7 @@
 import { useQuery, type UseQueryResult } from '@tanstack/react-query';
 import { apiFetchList, type ListParams, type ResourceList, type ResourceInput, HttpError, apiFetch, apiFetchRaw } from '../api';
 import { type Document } from '../models/document';
+import { useEffect, useMemo } from 'react';
 
 export type DocumentTypeInput = ResourceInput<DocumentType>;
 
@@ -20,14 +21,32 @@ export function useDocument(id: string | number, options = {}): UseQueryResult<D
   });
 }
 
-export function useDocumentThumbnail(id: string | number, options = {}): UseQueryResult<string, HttpError> {
-  return useQuery({
+export function useDocumentThumbnail(id: string | number, options = {}): UseQueryResult<string | undefined, HttpError> {
+  const query = useQuery<Blob, HttpError>({
     queryKey: ['document', 'get', {id}, 'thumbnail'],
     enabled: !!id,
     ...options,
     queryFn: async () => {
       const res = await apiFetchRaw(`api/v1/documents/${id}/thumbnail`);
-      return URL.createObjectURL(await res.blob());
+      return await res.blob();
     }
   });
+
+  const objectUrl = useMemo(() => {
+    if (!query.data) return undefined;
+    return URL.createObjectURL(query.data);
+  }, [query.data]);
+
+  useEffect(() => {
+    return () => {
+      if (objectUrl) {
+        URL.revokeObjectURL(objectUrl);
+      }
+    };
+  }, [objectUrl]);
+
+  return {
+    ...query,
+    data: objectUrl,
+  } as unknown as UseQueryResult<string | undefined, HttpError>;
 }
