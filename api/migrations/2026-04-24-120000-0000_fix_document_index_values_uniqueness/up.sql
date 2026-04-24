d01871c4a75a05bc01919c80904c8539d1116c65
Joe Khoobyar <fourheads@gmail.com>
2026-04-24 18:37:21 -0400

    autofile-api: fix for older postgresql

--- api/migrations/2026-04-24-120000-0000_fix_document_index_values_uniqueness/up.sql
+++ api/migrations/2026-04-24-120000-0000_fix_document_index_values_uniqueness/up.sql
@@ -3,4 +3,4 @@ ALTER TABLE document_index_values
 
 ALTER TABLE document_index_values
     ADD CONSTRAINT document_index_values_template_parent_value
-    UNIQUE NULLS NOT DISTINCT (document_index_template_id, parent_id, value);
+    UNIQUE (document_index_template_id, parent_id, value);
