d3425917ba43fdeaafa7127345fd48f91cc1547d
Joe Khoobyar <fourheads@gmail.com>
2026-04-26 17:25:37 -0400

    autofile: fix nulls not distinct in index values

---
+++ api/migrations/2026-04-26-120000-0000_fix_index_values/up.sql
@@ -0,0 +1,6 @@
+ALTER TABLE document_index_values
+    DROP CONSTRAINT IF EXISTS document_index_values_template_parent_value;
+
+ALTER TABLE document_index_values
+    ADD CONSTRAINT document_index_values_template_parent_value
+    UNIQUE NULLS NOT DISTINCT (document_index_template_id, parent_id, value);
